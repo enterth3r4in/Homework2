47c22ad239ecc28100ceae9c245fd4a4597056bd
enterth3r4in <believe.the.rain@gmail.com>
2016-01-20 12:15:46 -0500

    Project Update
    Finish at home

---
+++ HomeworkTwo/HomeworkTwo/CarDistance.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double carGasAmount = 20;
+	double townDrivingDistance = 21.5;
+	double highwayDrivingDistance = 26.8;
+
+	double distanceTown, distanceHighway;
+
+	distanceTown = carGasAmount * townDrivingDistance;
+	distanceHighway = carGasAmount * highwayDrivingDistance;
+
+	cout << "";
+
+	return 0;
+}
